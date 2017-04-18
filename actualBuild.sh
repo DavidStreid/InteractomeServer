3d378e4d2ccbdcaea30beba02bb4cef76a6e8c3d
David Streid <dcs2153@columbia.edu>
2017-04-17 18:16:16 -0700

    actualBuild

---
+++ actualBuild.sh
@@ -0,0 +1,17 @@
+# build project
+mvn clean install
+# Copy servlet .xmls to WEB-INF for .war packaging
+cp src/main/webapp/WEB-INF/*.xml /Users/Bike_Thoughts/code/InteractomeProject/target/Interactome/WEB-INF 
+rm target/Interactome.war
+cd target
+# Package .war
+jar -cvf Interactome.war Interactome/WEB-INF 
+rm -rf Interactome
+rm -rf /Users/Bike_Thoughts/code/apache-tomcat-8.5.13/webapps/Interactome
+rm /Users/Bike_Thoughts/code/apache-tomcat-8.5.13/webapps/Interactome.war
+# Copy .war to tomcat
+cp Interactome.war /Users/Bike_Thoughts/code/apache-tomcat-8.5.13/webapps
+cd /Users/Bike_Thoughts/code/apache-tomcat-8.5.13/webapps
+jar -xf Interactome.war
+# META-INF gets generated...
+rm -rf META-INF
