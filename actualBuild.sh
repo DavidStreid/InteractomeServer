48e6df633068e9668144d1cd91106540e964e4da
David Streid <dcs2153@columbia.edu>
2017-04-18 10:33:54 -0700

    fixed build script

--- actualBuild.sh
+++ actualBuild.sh
@@ -1,17 +1,13 @@
 # build project
-mvn clean install
+mvn package
 # Copy servlet .xmls to WEB-INF for .war packaging
 cp src/WEB-INF/*.xml /Users/Bike_Thoughts/code/InteractomeProject/target/Interactome/WEB-INF 
-rm target/Interactome.war
-cd target
+cd target/Interactome
 # Package .war
-jar -cvf Interactome.war Interactome/WEB-INF 
-rm -rf Interactome
-rm -rf /Users/Bike_Thoughts/code/apache-tomcat-8.5.13/webapps/Interactome
-rm /Users/Bike_Thoughts/code/apache-tomcat-8.5.13/webapps/Interactome.war
+jar -cvf Interactome.war WEB-INF 
+cd ..
 # Copy .war to tomcat
+rm /Users/Bike_Thoughts/code/apache-tomcat-8.5.13/webapps/Interactome.war
 cp Interactome.war /Users/Bike_Thoughts/code/apache-tomcat-8.5.13/webapps
-cd /Users/Bike_Thoughts/code/apache-tomcat-8.5.13/webapps
-jar -xf Interactome.war
-# META-INF gets generated...
-rm -rf META-INF
+# cd /Users/Bike_Thoughts/code/apache-tomcat-8.5.13/webapps
+# jar -xf Interactome.war
