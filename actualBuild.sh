c55154315e56e71757f733eaf436bdc11c5d63e3
David Streid <dcs2153@columbia.edu>
2017-04-21 20:22:29 -0700

    modified build script

--- actualBuild.sh
+++ actualBuild.sh
@@ -1,13 +1,11 @@
 # build project
 mvn package
 # Copy servlet .xmls to WEB-INF for .war packaging
-cp src/WEB-INF/*.xml /Users/Bike_Thoughts/code/InteractomeProject/target/Interactome/WEB-INF 
-cd target/Interactome
+cp src/WEB-INF/*.xml target/Interactome/WEB-INF 
+cd target/Interactome 
 # Package .war
-jar -cvf Interactome.war WEB-INF 
+jar -cvf Interactome.war WEB-INF .ebextensions
 cd ..
 # Copy .war to tomcat
 rm /Users/Bike_Thoughts/code/apache-tomcat-8.5.13/webapps/Interactome.war
 cp Interactome.war /Users/Bike_Thoughts/code/apache-tomcat-8.5.13/webapps
-# cd /Users/Bike_Thoughts/code/apache-tomcat-8.5.13/webapps
-# jar -xf Interactome.war
